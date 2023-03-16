90b8ccf72e5e96e09c2ad2ba3f04597da03735ef
Ganesh Suryawanshi <123467097+CreativeGanesh0635@users.noreply.github.com>
2023-03-16 20:10:34 +0530

    my_commit_(16/03)

---
+++ MEVN/VueJS/hello-world/src/components/MyVueFile.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  mycomment: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <!-- My FIle  -->
+  <div class="container">
+    <h3>I name is Ganesh Suni Suryawanshi</h3>
+  </div>
+</template>
+
+<style scoped>
+
+.greeting h3 {
+  text-align: center;
+}
+h3 {
+  color: green;
+}
+</style>
