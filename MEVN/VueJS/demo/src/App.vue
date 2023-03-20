97aa04e5dca36c61311384cb99e394940c5d1774
Ganesh Suryawanshi <123467097+CreativeGanesh0635@users.noreply.github.com>
2023-03-20 20:06:11 +0530

    commit_(20/03)

---
+++ MEVN/VueJS/demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <demo msg="Welcome to My Page"/>
+</template>
+
+<script>
+import demo from './components/demo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    demo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
