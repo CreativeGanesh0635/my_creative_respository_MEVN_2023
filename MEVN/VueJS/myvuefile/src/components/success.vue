ecbdd185c6882c938cbc9b282984834ae2f065bd
Ganesh Suryawanshi <123467097+CreativeGanesh0635@users.noreply.github.com>
2023-03-30 21:26:31 +0530

    my_commit(30/03)

---
+++ MEVN/VueJS/myvuefile/src/components/success.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>You have Submitted form, Successfully !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  text-align: center;
+}
+h1 {
+  color: rgb(71, 224, 135);
+}
+</style>
